4195de5dedcc000c30b663131aed6d53a539681b
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2022-11-10 17:10:51 -0800

    Added demo.js and index.html for Challenge 1

---
+++ lesson04/lesson04/demo.js
@@ -0,0 +1,15 @@
+function numberAdder() {
+  let input = prompt('Enter a number');
+  let numberToAdd = 2;
+  let result = 0;
+
+  result = numberToAdd + Number(input);
+
+  alert(result);
+}
+function squareAndAlert() {
+  let number = prompt('Enter a number');
+  let result =0;
+  result = number*number;
+  alert(result);
+}
